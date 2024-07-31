9d7d930db0e2a91d52a4af3bbae1eb284b4bb0d2
senior-devs-doom <75387713+senior-devs-doom@users.noreply.github.com>
2024-07-31 15:09:02 +0200

    Add files via upload

---
+++ mySQL.vb
@@ -0,0 +1,10 @@
+﻿Imports Microsoft.VisualBasic
+
+Namespace mySQL
+    Public Class mySQL
+        Public Shared Sub hi()
+            MessageBox.Show("hi, they deleted modules from visual studio, it is what it is.")
+        End Sub
+    End Class
+End Namespace
+
